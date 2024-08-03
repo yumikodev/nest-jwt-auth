b73c524a3e54d0fbfb71c2eb162b78b679498947
Yasu Yumiko <yumikokun0828@gmail.com>
2024-08-03 15:30:51 -0500

    fix: UserId decorator

--- src/auth/user-id.decorator.ts
+++ src/auth/user-id.decorator.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 
-export const UserId = () =>
-  createParamDecorator<unknown, ExecutionContext, string>((_data, ctx) => {
+export const UserId = createParamDecorator<unknown, ExecutionContext, string>(
+  (_data, ctx) => {
     const req = ctx.switchToHttp().getRequest();
     return req["userId"];
-  });
+  },
+);
