5e3041736f901ce31ded332281ee76caa38b8867
Yasu Yumiko <yumikokun0828@gmail.com>
2024-08-03 00:01:06 -0500

    feat: use @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import env from "./config/env";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [env],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
